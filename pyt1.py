16acf4a9b0503f304f3619f1d5053c489f941ea8
Franciszek Turowski <franciszekturowski98@gmail.com>
2024-12-17 14:36:37 +0100

    keep learning

---
+++ pyt1.py
@@ -0,0 +1,45 @@
+# channel_name = 'Jak nauczyć się programowania'
+# print(channel_name.split(" "))
+
+# text = 'Programuj od podstaw'
+
+# print(text[-1])
+
+# print(len(text))
+
+# def add (a,b):
+#     print(a + b)
+    
+    
+# a = 8
+# b = '3'
+
+# print(a + int(b))  
+
+# age = int(input("Ile masz lat? :"))
+
+# if age >= 18:
+#     print("Jestes dorosły!")
+# else: print("Jesteś niepełnoletni!")    
+
+light = input("Jakie jest światło? (zielone, żółte,czerwone)")
+
+# if light == 'zielone':
+#     print("Możesz jechać")
+# elif light == 'żółte':
+#     print("Przygotuj się")
+# elif light == 'czerwone':
+#     print("Stój!")
+# else: print("Niewłaściwt kolor")    
+
+# match light:
+#     case 'zielone':
+#         print('Możesz jechać')
+#     case 'żółte':
+#         print("Przygotuj się")
+#     case 'czerwone':
+#         print('Stój!!!')
+#     case _:
+#         print("Nie znam takiego koloru!")
+        
+        
